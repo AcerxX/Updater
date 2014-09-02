b1ad492873b4cb6269fc9fdf1e7a615e2b156a4b
Alexandru MIHAI <alexandru.mihai0808@cti.pub.ro>
2014-09-02 17:21:39 +0300

    [r2] Implemented Updater Script

--- src/updater/Updater.java
+++ src/updater/Updater.java
@@ -5,14 +5,13 @@
  */
 package updater;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -22,20 +21,30 @@ public class Updater {
 
     /**
      * @param args the command line arguments
+     * @throws java.net.MalformedURLException
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MalformedURLException, IOException {
+
+        /* Getting the new app */
+        URL website = new URL("http://aica.org.ro/images/FTP/AcerX_File_Transfer_App.jpg");
+        ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+        FileOutputStream fos = new FileOutputStream("AcerX_File_Transfer_App");
+        fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+
+        /* Removing old app */
+        File oldApp = new File("AcerX_File_Transfer_App.jar");
+        oldApp.delete();
+
+        /* Replace with the new app */
+        File newApp = new File("AcerX_File_Transfer_App");
+        newApp.renameTo(oldApp);
+
+        // Start the Main App
+        ProcessBuilder mainApp = new ProcessBuilder("", "-jar", "AcerX_File_Transfer_App.jar");
+        Process p = mainApp.start();
         
-        try {
-            URL website = new URL("http://aica.org.ro/images/FTP/AcerX_File_Transfer_App.jpg");
-            ReadableByteChannel rbc = Channels.newChannel(website.openStream());
-            FileOutputStream fos = new FileOutputStream("AcerX_File_Transfer_App.jar");
-            fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
-        } catch (MalformedURLException ex) {
-            Logger.getLogger(Updater.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(Updater.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
+        System.exit(0);
+
     }
 
 }
